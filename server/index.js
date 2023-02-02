7a301faf003b0ce0a66268893bbb2aed224d8d3c
Vihimuro <vihi1208@gmail.com>
2023-02-02 13:03:01 -0300

    mocking data to the app

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
+//data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 /* CONFIGURATION */
 dotenv.config();
 const app = express();
@@ -32,5 +36,8 @@ mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   }).then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* RUN JUST ONCE*/
+    //User.insertMany(dataUser);
   }).catch((error) => console.log(`{error} did not connect`));
