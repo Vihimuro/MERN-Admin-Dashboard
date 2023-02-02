f2193e1e3806830059533c9e52f41b4f3cf5a4fe
Vihimuro <vihi1208@gmail.com>
2023-02-02 12:54:12 -0300

    setting user

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -20,6 +20,8 @@ import {
 } from "@mui/material";
 
 const Navbar = ({
+  isSidebarOpen,
+  setIsSidebarOpen,
 }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
@@ -35,7 +37,7 @@ const Navbar = ({
       <Toolbar sx={{ justifyContent: "space-between" }}>
         {/* Left-side*/}
         <FlexBetween>
-          <IconButton onClick={() => console.log("open/close sidebar")}>
+          <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
             <MenuIcon />
           </IconButton>
           <FlexBetween
